ebe85da9ff242fe96289b8f6e4bbb9909e4c41ed
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2022-05-04 23:43:10 +0200

    home link as logo in router and styling to body and nav

--- gamelog/src/components/header.jsx
+++ gamelog/src/components/header.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "/Users/benjamin/Documents/GameLog/GameLog/gamelog/src/styles/navbar.scss";
-import logo from "/Users/benjamin/Documents/GameLog/GameLog/gamelog/src/gamelog-icon.png";
+import logo from "/Users/benjamin/Documents/GameLog/GameLog/gamelog/src/images/gamelog-icon.png";
 
 const Header = () => {
   return (
     <div className="navigationbar">
       <div className="nav-logo">
-        <img src={logo} alt="logo" />
+        <li>
+          <Link to="/">
+            {" "}
+            <img src={logo} alt="logo" />
+          </Link>
+        </li>
       </div>
       <div className="nav-menu">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
         <li>
           <Link to="/games">Games</Link>
         </li>
